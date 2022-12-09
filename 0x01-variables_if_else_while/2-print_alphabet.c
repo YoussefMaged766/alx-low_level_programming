721054ab40908c55133e676d7a1b3b0b2bbc3587
YoussefMaged766 <yoer766@gmail.com>
2022-12-09 11:46:12 -0800

    loops

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+ *
+ */
+
+int main(void)
+{
+char alphabet = 'a';
+
+for (alphabet = 'a'; alphabet <= 'z' ; alphabet++){
+	putchar(alphabet);
+}
+putchar('/n');
+return (0);
+}
