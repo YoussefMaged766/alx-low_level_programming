b8ade48100321373a488894ae4ddb0342e761bfd
YoussefMaged766 <yoer766@gmail.com>
2022-12-09 12:18:23 -0800

    loops

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print alphabet in reverse
+ * Return: Zero value
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		putchar(n);
+	putchar('\n');
+	return (0);
+}
