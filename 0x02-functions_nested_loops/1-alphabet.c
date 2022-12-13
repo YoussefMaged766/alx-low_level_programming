17177dc215617c681509cfe4be3fe3794aa94879
YoussefMaged7666 <yoer766@gmail.com>
2022-12-13 12:37:40 -0800

    11

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int c;
+
+	for(i = 'a'; i <= 'z';i++ )
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+}
